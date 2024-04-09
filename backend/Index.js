c970c8ea87fc74c93974c4deb3bb856d18c92d04
pnkkumar123 <pankajkanwar420@gmail.com>
2024-04-09 16:36:35 +0530

    completed personal portfolio website

--- backend/Index.js
+++ backend/Index.js
@@ -1,45 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+
 const cors = require('cors');
-const route = require('./api/routes/Route.js') ;
-const routes =  require ('./api/routes/Auth.js');
-const path = require('path')
+const route = require('./api/routes/Route.js');
+const routes = require('./api/routes/Auth.js');
+const path = require('path');
+dotenv.config();
+const envFilePath = path.resolve(__dirname, '.env');
 
+// Load environment variables from the custom .env file
+dotenv.config({ path: envFilePath });
+ 
 
-require('dotenv').config();
+mongoose.connect(process.env.MONGO_URL, {
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-    console.log("connected to mongodb");
-}).catch((error)=>{
-    console.log(error)
-})
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("MongoDB connection error:", error);
+});
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.listen("8000",()=>{
-    console.log("port connected")
-})
-app.use("/project",route)
-app.use("/admin",routes)
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-// deplpyment
+const PORT = 8000; // Use the environment port or default to 8000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
+app.use("/project", route);
+app.use("/admin", routes);
+
+// Deployment
 const __dirname1 = path.resolve();
 
-if("production" === "production"){
-    app.use(express.static(path.join(__dirname1,"/Portfolio_website")));
-     
-    app.get("*",(req,res)=>
-     res.sendFile(path.resolve(__dirname1,"Portfolio_website","dist","index.html"))
-   
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/Portfolio_website/dist")));
   
-)
-
-}else{
-    app.get("/",(req,res)=>{
-        res.send("api is running..")
-    })
+    app.get("*", (req, res) => 
+      res.sendFile(path.resolve(__dirname1, "Portfolio_website", "dist", "index.html"))
+    );
+  }
+else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
